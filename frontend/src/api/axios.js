1b78644721f84946582b3f902515100c1e4c44ed
shreyashgit-01 <sasmalshreyash@gmail.com>
2026-08-03 17:38:16 +0530

    Fix frontend API endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://insurance-management-platform-production.up.railway.app/api/",
+  baseURL: "http://127.0.0.1:8000/api/",
   headers: {
     "Content-Type": "application/json",
   },
